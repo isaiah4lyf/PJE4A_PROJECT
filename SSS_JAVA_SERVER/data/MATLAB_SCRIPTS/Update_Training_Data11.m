dc9695493733bcf18746fb2a93ef45dda4b351ac
isaiah4lyf <isaiah.ramafalo.5@gmail.com>
2018-07-20 16:06:00 +0200

    Added try and catch at the matlab scriptsw

--- SSS_JAVA_SERVER/data/MATLAB_SCRIPTS/Update_Training_Data11.m
+++ SSS_JAVA_SERVER/data/MATLAB_SCRIPTS/Update_Training_Data11.m
@@ -1,65 +1,80 @@
+try
+	%% Measure Distance from Stereo Camera to a Face
+	%% Load stereo parameters.
 
-%% Measure Distance from Stereo Camera to a Face
-%% Load stereo parameters.
+	% Copyright 2015 The MathWorks, Inc.
 
-% Copyright 2015 The MathWorks, Inc.
-
-load('webcamsSceneReconstruction.mat');
-%% Read in the stereo pair of images.
-
-
-%% Undistort the images.
-image = undistortImage(I1,stereoParams.CameraParameters1);
-
-%% Detect a face in both images.
-faceDetector = vision.CascadeObjectDetector;
-face1 = step(faceDetector,image);
+	load('webcamsSceneReconstruction.mat');
+	%% Read in the stereo pair of images.
 
 
+	%% Undistort the images.
+	image = undistortImage(I1,stereoParams.CameraParameters1);
 
-I = imcrop(I1,face1);
-im=rgb2gray(I);
-J = imresize(im, 5);
-%imshowpair(J, I2, 'montage');
-
-
-%% Read image and detect interest points.
-
-ptsOriginal = detectSURFFeatures(J);
-   
-%% Display locations of interest in image.
-%imshow(J); hold on;
-%plot(ptsOriginal.selectStrongest(100));
-imwrite(J,image_Path)
-
-
-[featuresOriginal,validPtsOriginal] = ...
-            extractFeatures(J,ptsOriginal);
-
-   
-[m,n] = size(featuresOriginal);
-A = [];
-
-for i=1:m
-    A = [A;user_ID];
-end
+	%% Detect a face in both images.
+	faceDetector = vision.CascadeObjectDetector;
+	face1 = step(faceDetector,image);
 
 
 
+	I = imcrop(I1,face1);
+	im=rgb2gray(I);
+	J = imresize(im, 5);
+	%imshowpair(J, I2, 'montage');
 
-mat = [double(A) double(featuresOriginal)];
+
+	%% Read image and detect interest points.
+
+	ptsOriginal = detectSURFFeatures(J);
+	   
+	%% Display locations of interest in image.
+	%imshow(J); hold on;
+	%plot(ptsOriginal.selectStrongest(100));
+	imwrite(J,image_Path)
+
+
+	[featuresOriginal,validPtsOriginal] = ...
+				extractFeatures(J,ptsOriginal);
+
+	   
+	[m,n] = size(featuresOriginal);
+	A = [];
+
+	for i=1:m
+		A = [A;user_ID];
+	end
 
 
 
-if exist(path, 'file')
-  % File exists.  Do stuff....
-  file2 = load(path);
-  new_File = [file2.new_File;mat];
-  save(path2,'new_File');
-else
-  % File does not exist.
-  new_File = [mat];
-  save(path2,'new_File');
+
+	mat = [double(A) double(featuresOriginal)];
+
+
+
+	if exist(path, 'file')
+	  % File exists.  Do stuff....
+	  file2 = load(path);
+	  new_File = [file2.new_File;mat];
+	  save(path2,'new_File');
+	else
+	  % File does not exist.
+	  new_File = [mat];
+	  save(path2,'new_File');
+	end
+
+catch ME
+    switch ME.identifier
+        case 'MATLAB:UndefinedFunction'
+            warning('Function is undefined.  Assigning a value of NaN.');
+            %a = NaN;
+        case 'MATLAB:scriptNotAFunction'
+            warning(['Attempting to execute script as function. '...
+                'Running script and assigning output a value of 0.']);
+            notaFunction;
+            %a = 0;
+        otherwise
+            warning('Invalid Image....');
+    end
 end
 
 
