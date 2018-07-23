d87d18b1723c2c02ffe4a5dbbf634db885ea6122
isaiah4lyf <isaiah.ramafalo.5@gmail.com>
2018-07-23 08:40:49 +0200

    Deleted unnecessary files and fixed some functionalityu

--- SSS_JAVA_SERVER/data/MATLAB_SCRIPTS/Update_Training_Data11.m
+++ SSS_JAVA_SERVER/data/MATLAB_SCRIPTS/Update_Training_Data11.m
@@ -17,50 +17,7 @@ try
 
 
 
-	I = imcrop(I1,face1);
-	im=rgb2gray(I);
-	J = imresize(im, 5);
-	%imshowpair(J, I2, 'montage');
 
-
-	%% Read image and detect interest points.
-
-	ptsOriginal = detectSURFFeatures(J);
-	   
-	%% Display locations of interest in image.
-	%imshow(J); hold on;
-	%plot(ptsOriginal.selectStrongest(100));
-	imwrite(J,image_Path)
-
-
-	[featuresOriginal,validPtsOriginal] = ...
-				extractFeatures(J,ptsOriginal);
-
-	   
-	[m,n] = size(featuresOriginal);
-	A = [];
-
-	for i=1:m
-		A = [A;user_ID];
-	end
-
-
-
-
-	mat = [double(A) double(featuresOriginal)];
-
-
-
-	if exist(path, 'file')
-	  % File exists.  Do stuff....
-	  file2 = load(path);
-	  new_File = [file2.new_File;mat];
-	  save(path2,'new_File');
-	else
-	  % File does not exist.
-	  new_File = [mat];
-	  save(path2,'new_File');
-	end
 
 catch ME
     switch ME.identifier
@@ -74,7 +31,49 @@ catch ME
             %a = 0;
         otherwise
             warning('Invalid Image....');
+			status = 'false';
     end
 end
 
+if status == 'true'
 
+	I = imcrop(I1,face1);
+	im=rgb2gray(I);
+	J = imresize(im, 5);
+	%imshowpair(J, I2, 'montage');
+
+
+	%% Read image and detect interest points.
+
+	ptsOriginal = detectSURFFeatures(J);
+	   
+	status = 'true';
+	%% Display locations of interest in image.
+	%imshow(J); hold on;
+	%plot(ptsOriginal.selectStrongest(100));
+	imwrite(J,image_Path)
+
+	[featuresOriginal,validPtsOriginal] = ...
+				extractFeatures(J,ptsOriginal);
+	[m,n] = size(featuresOriginal);
+	A = [];
+
+	for i=1:m
+		A = [A;user_ID];
+	end
+
+	mat = [double(A) double(featuresOriginal)];
+
+	if exist(path, 'file')
+	  % File exists.  Do stuff....
+	  file2 = load(path);
+	  new_File = [file2.new_File;mat];
+	  save(path2,'new_File');
+	else
+	  % File does not exist.
+	  new_File = [mat];
+	  save(path2,'new_File');
+	end
+else
+	response = 'Code doesnt Work'
+end
--- SSS_MOBILE_APP/app/src/main/java/com/example/isaia/sss_mobile_app/Reg_User.java
+++ SSS_MOBILE_APP/app/src/main/java/com/example/isaia/sss_mobile_app/Reg_User.java
@@ -1,44 +1,60 @@
 package com.example.isaia.sss_mobile_app;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.isaia.sss_mobile_app.SSS_CLIENT_FUNCTIONS.Insert_User;
 import com.example.isaia.sss_mobile_app.SSS_CLIENT_FUNCTIONS.Pred_User;
 
 public class Reg_User extends AppCompatActivity {
-    private TextView txvResult;
+
+    private EditText user_Name;
+    private EditText Password;
+    private String User_Name_String;
+    private String Password_String;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg__user);
-
         //Registering a user
-        txvResult = (TextView) findViewById(R.id.textView);
+        user_Name = (EditText) findViewById(R.id.User_Name);
+        Password = (EditText) findViewById(R.id.Password);
 
-        Thread thread = new Thread(new Runnable() {
 
-            @Override
-            public void run() {
+        final Button reg = (Button) findViewById(R.id.Sign_Up);
+        reg.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        User_Name_String = user_Name.getText().toString();
+                        Password_String = Password.getText().toString();
+                        Thread thread = new Thread(new Runnable() {
 
-                //txvResult.setText("Hello");
+                            @Override
+                            public void run() {
+                                try {
+                                    reg_us tast = new reg_us();
+                                    tast.execute();
 
-                try {
-                    reg_us tast = new reg_us();
-                    tast.execute();
+                                }
+                                catch (Exception ex)
+                                {
+                                    //txvResult.setText(ex.toString());
+                                }
+                            }
+                        });
 
+                        thread.start();
+                    }
                 }
-                catch (Exception ex)
-                {
-                    txvResult.setText(ex.toString());
-                }
-            }
-        });
-
-        thread.start();
-
+        );
     }
     private class reg_us extends AsyncTask<String, Void, String> {
 
@@ -50,12 +66,12 @@ public class Reg_User extends AppCompatActivity {
         protected String doInBackground(String... urls) {
 
             Insert_User insert_user = new Insert_User();
-            return insert_user.Do_The_work("Isaiah");
+            return insert_user.Do_The_work(User_Name_String);
         }
         @Override
         protected void onPostExecute(String result) {
             //if you started progress dialog dismiss it here
-            txvResult.setText(result);
+            //txvResult.setText(result);
         }
     }
 
